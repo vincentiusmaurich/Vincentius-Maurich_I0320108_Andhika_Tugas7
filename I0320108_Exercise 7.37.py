b3e761861804a8ddd004a91302b3198772ba7524
Vincentius Maurich <maurichkian@gmail.com>
2021-04-16 08:38:46 +0700

    Tugas 7

--- I0320108_Exercise 7.37.py
+++ I0320108_Exercise 7.37.py
@@ -1,4 +1,5 @@
 a = [10,40,30,10,70,40]
+
 print("a awal= ", a)
 a.sort()
 print("a sesudah di urutkan= ", a)
--- I0320108_Exercise 7.38.py
+++ I0320108_Exercise 7.38.py
@@ -1,4 +1,5 @@
 a = [10,40,30,10,70,40]
 b = sorted(a)
+
 print("a= ", a)
 print("b= ", b)
