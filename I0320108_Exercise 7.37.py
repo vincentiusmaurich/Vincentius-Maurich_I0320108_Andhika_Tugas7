8df5fbe341b2b07ee44b4f3e2362fcc6d44daaee
Vincentius Maurich <maurichkian@gmail.com>
2021-04-16 08:38:00 +0700

    Tugas 7

--- I0320108_Exercise 7.37.py
+++ I0320108_Exercise 7.37.py
@@ -2,8 +2,3 @@ a = [10,40,30,10,70,40]
 print("a awal= ", a)
 a.sort()
 print("a sesudah di urutkan= ", a)
-
-a = [10,40,30,10,70,40]
-b = sorted(a)
-print("a= ", a)
-print("b= ", b)
--- I0320108_Soal2_Tugas7.py
+++ I0320108_Soal2_Tugas7.py
@@ -1,1 +1,26 @@
+nilai_siswa = input("Masukkan nilai siswa (dipisahkan dengan spasi) : ").split()
 
+#Konversi menjadi tipe int
+for i in range(len(nilai_siswa)):
+    nilai_siswa[i] = int(nilai_siswa[i])
+
+#Mencari rata-rata nilai
+rerata = sum(nilai_siswa)/len(nilai_siswa)
+
+print("Nilai yang diinput : ", nilai_siswa)
+print('='*30)
+
+import math
+#Fungsi Numerik 1 = mencari nilai tertinggi
+print("Nilai yang paling tinggi adalah ", max(nilai_siswa))
+
+#Fungsi Numerik 2 = mencari nilai terendah
+print("Nilai yang paling rendah adalah ", min(nilai_siswa))
+
+print("Rata-rata nilai siswa adalah ", rerata)
+
+#Fungsi Numerik 3 = pembulatan ke atas
+print("Rata-rata nilai dengan pembulatan ke atas adalah ", math.ceil(rerata))
+
+#Fungsi Numerik 4 = pembulatan ke bawah
+print("Rata-rata nilai dengan pembulatan ke bawah adalah ", math.floor(rerata))
